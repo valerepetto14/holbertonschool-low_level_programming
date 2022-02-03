b7f6dc9e677443202bffd9b6c310af0f36ecf2e9
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-03 10:31:12 -0800

    9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include <unistd.h>
+/**
+ *  * main - Entry point
+ *   * Return: Always 1 (Success)
+ *    */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-|ZZ10-19\n", 61);
+	return(0);
+}
