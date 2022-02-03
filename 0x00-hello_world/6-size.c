051adc1fd54f4a596a464849aa885f20552cf807
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-03 09:24:01 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
  **/
 int main(void)
 {
-printf("Size of a char: " "%zu" "byte(s)\n", (unsigned long)sizeof(char));
+printf("Size of a char: " "%zu" " byte(s)\n", (unsigned long)sizeof(char));
 printf("Size of an int: " "%zu" " byte(s)\n", (unsigned long)sizeof(int));
 printf("Size of a long int: ""%zu" " byte(s)\n", (unsigned long)sizeof(long int));
 printf("Size of a long long int: ""%zu"" byte(s)\n", (unsigned long)sizeof(long long int));
