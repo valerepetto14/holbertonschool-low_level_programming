25ad8f50d1c7aa59a874c892931c1097dccd9e59
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-03 07:21:57 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include "stdio.h"
 /*
  *print in text in display
- *
- * 
 */
 int main(void)
 {
-printf("Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }
