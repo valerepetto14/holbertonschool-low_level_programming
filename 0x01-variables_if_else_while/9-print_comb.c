1089252d51eeb54cf796f6d465b93b7baa4af967
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-04 11:05:45 -0800

    12

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ *  *  *  *  main - Entry point
+ *   *   *  * Return: Always 1 (Success)
+ *    *    *    *    *      **/
+int main(void)
+{
+int x;
+
+for (x = 0; x <= 9; x++)
+{
+putchar(x + '0');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
