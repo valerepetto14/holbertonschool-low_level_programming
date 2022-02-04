f0e1ec2f4322d213736ba00b35206976e88a07a2
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-04 08:49:52 -0800

    12

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ *  *  *   *  main - Entry point
+ *   *   *     * Return: Always 1 (Success)
+ *    *    *      **/
+int main(void)
+{
+	char x;
+
+for (x = 'a'; x <= 'z'; x++)
+}
+		if (x == 'q' || x == 'e')
+			break;
+	else
+		putchar(x);
+}
+putchar('\n');
+return (0);
+}
