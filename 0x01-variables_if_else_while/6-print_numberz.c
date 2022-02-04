2d8908c3fbb5fcdec832dfdcd89c566966a08b93
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-04 09:26:05 -0800

    12

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ *  *  *  *  *   *  main - Entry point
+ *   *   *   *   *     * Return: Always 1 (Success)
+ *    *    *    *    *      **/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10 ; x++)
+{
+	putchar(x + 'x');
+}
+}
