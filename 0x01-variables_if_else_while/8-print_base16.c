7ba0841483cae228242caf82c73b8ce5130b9f97
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-04 10:28:03 -0800

    12

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ *  *  *  main - Entry point
+ *   *  * Return: Always 1 (Success)
+ *    *    *    *      **/
+int main(void)
+{
+int x;
+
+char a;
+
+for (x = 0; x <= 9; x++)
+putchar(x + '0');
+
+for (a = 'a'; x <= 'f'; x++)
+putchar(a);
+
+	putchar('\n');
+}
