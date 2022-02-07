1d012839b884acfeef062d404903c962c31a2ec0
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-07 10:15:05 -0800

    12

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,11 +12,19 @@ void times_table(void)
 	for(a = 0; a <= 9; a++)
 	{
 		 for(b = 0; b <= 9; b++)
-		{
+		{	
+			if(b != 9)
+			{
 			c= a * b;
 			_putchar(c + '0');
 			_putchar(',');
 			_putchar(' ');
+			}
+			else
+			{
+				putchar(c + '0');
+			}
 		}
+
 	}
 }
