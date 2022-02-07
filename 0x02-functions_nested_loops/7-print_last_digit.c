ddede0e1672dcd5485eca99a133f5e879d782957
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-07 09:45:20 -0800

    12

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,14 +7,12 @@
 int print_last_digit(int c)
 {
 	int l= c % 10;
-	if ((l % 10) < 0)
+
+	if (l < 0)
 	{	
 		l=l * -1;
 		_putchar(l);
 	}
-	else
-	{
-		_putchar(l);
-	}
-	return 0;
+		_putchar('0' + l);
+	return l;
 }
