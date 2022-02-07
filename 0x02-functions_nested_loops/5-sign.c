8ac04a9435ef09a3e6f8951307707456cd34e713
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-07 08:44:51 -0800

    12

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *  print_Sign - imprime el signo
+ *print_sign - dkdkdk
  *@n: n
  *Return:0
  **/
@@ -8,17 +8,17 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-	 _putchar('-' + '0');
-	 return (-1);
+	_putchar('-');
+	return (-1);
 	}
 	else if (n > 0)
 	{
-		_putchar('+' + '0');
+		_putchar('+');
 		return (1);
 	}
 	else
 	{
-	  _puthcar(0)
-	  return (0);	  
+	  _putchar(0);
+		return (0);
 	}
 }
