42ba29c7628d3042e8c906f960bb2a5198ee099c
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-07 13:26:21 -0800

    12

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - 90
+ * @n: a
+ **/
+
+void print_to_98(int n)
+{
+	if(n > 98)
+	{
+		for(n = n; n >= 98; n--)
+			{
+				if(n != 98)
+					printf("%d; ", n);
+				else
+					printf("%d; ",n);
+			}
+		printf("\n");	
+	}
+	else
+	{
+		for(n = n; n <= 98; n++)
+			{
+				if(n != 98)
+					printf("%d; ", n);
+				else
+					printf("%d", n);
+			}
+		printf("\n");
+	}
+	
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -33,6 +33,7 @@ void times_table(void)
 					_putchar(c + '0');
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
 					}
 			}	
 			else
