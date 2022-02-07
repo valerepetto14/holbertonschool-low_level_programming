7b2e6c17b9e1e9a5ff274c6fb2d168abce592416
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-07 08:57:27 -0800

    12

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,9 @@
+/**
+ * _abs - math
+ * @c: dd
+ * Return: 0
+ **/
+int _abs(int c)
+{
+return (c % 10);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,3 +10,5 @@ int _islower(int c);
 int _isalpha(int c);
 
 int print_sign(int n);
+
+int _abs(int);
