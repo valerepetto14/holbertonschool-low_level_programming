0861e871cdb7c61b3413edfa183f55f90b154f89
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-07 16:24:08 -0800

    12

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,27 +7,26 @@
 
 void print_to_98(int n)
 {
-	if(n > 98)
+				if (n > 98)
 	{
-		for(n = n; n >= 98; n--)
+				for (n = n; n >= 98; n--)
 			{
-				if(n != 98)
+				if (n != 98)
 					printf("%d; ", n);
 				else
-					printf("%d; ",n);
+					printf("%d; ", n);
 			}
-		printf("\n");	
+		printf("\n");
 	}
 	else
 	{
-		for(n = n; n <= 98; n++)
+				for (n = n; n <= 98; n++)
 			{
-				if(n != 98)
+				if (n != 98)
 					printf("%d; ", n);
 				else
 					printf("%d", n);
 			}
 		printf("\n");
 	}
-	
 }
