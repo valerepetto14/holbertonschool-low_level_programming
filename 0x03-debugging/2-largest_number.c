bc39535210e5657710c573d165b03d1f1533ba3c
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-08 11:36:38 -0800

    12

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,15 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a >= b && c >= c)
 	{
 	largest = a;
 	}
-	else if (b > c && b > a)
+	else if (b >= a && b >= c)
 	{
 	largest = b;
 	}
-	else if (c > b && c > a)
+	else if (c >= a && c >= b)
 	{
 	largest = c;
 	}
--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -10,9 +10,9 @@ int main(void)
 {
         int a, b, c;
         int largest;
-
+	
         a = 972;
-        b = -98;
+        b = 1000;
         c = 0;
 
         largest = largest_number(a, b, c);
