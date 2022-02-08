0b2c4eedb7a0e7b123020973165b91277a7c44a5
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-07 17:32:49 -0800

    12

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,48 +12,35 @@ void times_table(void)
 		{
 			c = a * b;
 			if (b != 9)
-			{	
-				
 				if (c > 9)
 					{
 					numA = c % 10;
 					numB = (c / 10) % 10;
 					_putchar(numB + '0');
 					_putchar(numA + '0');
-					 _putchar(',');
-					 _putchar(' ');
-					}
-				else if(b == 0)
+					_putchar(',');
+					_putchar(' '); }
+				else if (b == 0)
 					{
 					_putchar(c + '0');
 					_putchar(',');
-					_putchar(' ');	
-					}	
+					_putchar(' '); }
 				else
 					{
 				 _putchar(' ');
 				_putchar(c + '0');
 				_putchar(',');
 				_putchar(' ');
-					}
-			}
+					}	}
 		else
 		{
 			if (c > 9)
 				{
-				numA = c % 10;
-				numB = (c / 10) % 10;
-				_putchar(numB + '0');
-				_putchar(numA + '0');
+				_putchar((c % 10) + '0');
+				_putchar((c / 10) % 10) + '0');
 				}
 			else
-			{
+				{
 				_putchar(' ');
 				_putchar(c + '0');
-				}
-	       	} 
-	}
-	_putchar('\n');
-}
-}
-
+				}	}	} _putchar('\n'); }	}
