2ac45f838e31836c702e35d1f38ea026d696c08f
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-09 07:44:46 -0800

    12

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,18 +6,9 @@
  **/
 int _isupper(int c)
 {
-	int i;
 
-	for (i = 'A'; i <= 'Z'; i++)
-	{
-		if (i == c)
-		{
-			break;
+	if(c >= 'A' && c < == 'Z')
 			return (1);
-		}
-
 		else
 			return (0);
-	}
-	return (0);
 }
