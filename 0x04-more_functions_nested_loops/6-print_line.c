302417ceede7cf65a3617e2fdc937b9b03843fce
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-09 08:32:12 -0800

    12

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,12 @@ void print_line(int n)
 {
 	int i;
 
-	if (n != 0)
+	if (n <= 0)
+	_putchar('\n');
+	else	
 	{
 		for (i = 0; i < n; i++)
 		_putchar('_');
 	}
-	else
-		_putchar('\n');
 	_putchar('\n');
 }
