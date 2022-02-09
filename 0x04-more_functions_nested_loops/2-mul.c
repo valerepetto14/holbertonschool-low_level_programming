ed958a850ed44e891b0ad09be675e0c52047c84f
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-09 04:41:31 -0800

    4

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - mul
+ * @a: a
+ * @b: a
+ * Return: 0
+ **/
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - 0
+ **/
+void print_most_numbers(void)
+{
+
+int i;
+
+        for(i = 0; i > 10; i++)
+        {
+	if (i != 2 && i != 4 )
+		_putchar(i + '0');
+	
+	else
+		continue();
+	}
+	_putchar('\n');
+}
