2fc4b865186ee1f65b245eff3c9cbfdd84cbe1a1
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-09 09:18:32 -0800

    12

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,9 +13,9 @@ int main(void)
 		printf("Buzz ");
 	else if ((a % 3) == 0 && (a % 5) == 0)
 		printf("FizzBuzz ");
-	else if ((a % 3) == 0 && (a % 5) != 0 )
+	else if ((a % 3) == 0)
 		printf("Fizz ");
-	else if ((a % 5) == 0 && (a % 3) != 0)
+	else if ((a % 5) == 0)
 		printf("Buzz ");
 	else
 		printf("%i ", a);
