454c22dae25954aff693f8711309dfdee924e0c7
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-11 02:34:39 -0800

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,14 +4,14 @@
 *@s: char
 **/
 void print_rev(char *s)
-{	
+{
 int i;
 
 	i = 0;
 		while (s[i] != '\0')
-			{
-				i++;
-			}
+		{
+			i++;
+		}
 		for (i = i - 1 ; i >= 0; i--)
 		{
 			_putchar (s[i]);
