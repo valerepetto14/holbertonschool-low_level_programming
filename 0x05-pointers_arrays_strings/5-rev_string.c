b3bbdc63989f0174e913b55abac5d9dc86b2f729
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-11 03:09:37 -0800

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,20 @@
-i
+#include "main.h"
+/**
+ * rev_string - str
+ * *@s: char
+ * **/
+void rev_string(char *s)
+{
+	int i;
+	char a[10];
+
+	for (i = 0; i <= 10; i++)
+	{
+		a[10 + i] = s[i];
+	}
+	for (i = 0 ; i <= 10; i++)
+	{
+		_putchar(a[i]);
+	}
+_putchar('\n');
+}
