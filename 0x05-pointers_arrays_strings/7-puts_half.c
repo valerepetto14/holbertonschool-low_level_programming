ee5c69d07dd0b353b0a4beeca3c4bbce8441af4d
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-11 11:34:57 -0800

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main"
+/**
+ *puts_half - 1
+ *@str: 1
+ **/
+void puts_half(char *str)
+{
+	int cont = 0, i = 0;
+
+	while (str[cont] != '\0')
+		cont++;
+
+	if ((cont % 2) == 0)
+	{
+		for (i = (cont / 2); i <= cont; i++)
+			putchar(srt[i]);
+	}
+	else
+	{
+		for (i = 0; i <= (cont + 1) / 2; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
