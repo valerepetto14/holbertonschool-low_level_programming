32e820289c30f3e56bca26808e7976d18ca07a92
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-10 23:41:34 -0800

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_rev - str
+*@s: char
+**/
+void print_rev(char *s)
+{
+	char *a;
+
+	a = s;
+	while (*a != 0)
+		*a++;
+
+		while (*a != 0)
+			}
+			_putchar(*a);
+			*a++;
+			}
+_putchar('\n');
+}
