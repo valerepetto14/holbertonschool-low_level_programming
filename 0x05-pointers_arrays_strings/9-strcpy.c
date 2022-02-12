51b34433836fbc7cdabf92254dcd5d905d7245ed
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-12 12:15:11 -0800

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,35 @@
 #include "main.h"
 /**
-*_strcpy - 
-*@dest:1
+ *_strcpy - dddI
+*@dest: 1
 *@src: 1
+* Return: dest
  **/
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0, a = 0;
+	int a = 0;
 
-  	while (src[i] != '\n')
-		i++;
-
-	for(a = 0; a <= i; a++)
+	for (a = 0; a <= _strlen(src); a++)
 		dest[a] = src[a];
 
 	return (dest);
+}
+/**
+ *  * _strlen - str
+ *   *@s: char
+ *    *Return: cont
+ *     **/
+int _strlen(char *s)
+{
+	int cont = 0;
+	char *a;
+
+	a = s;
+
+		while (*a != 0)
+		{
+			a++;
+			cont++;
+		}
+	return (cont);
 }
