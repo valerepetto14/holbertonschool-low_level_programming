42d8a896a1a9522cce82e32ad127b40c86e21c58
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-16 16:42:01 +0000

    todo

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/*
+ *print_number - eee
+ *@n: ddd
+ */
+void print_number(int n)
+{
+
+}
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,7 +9,7 @@ void reverse_array(int *a, int n)
 	int b = 0;
 	int AUX;	
 	n-=1;
-	for (b = 0; b < (n-1)/2 ; b++, n--)
+	for (b = 0; b < (n)/2 ; b++, n--)
 	{
 		AUX = a[n];
 		a[n] = a[b];
