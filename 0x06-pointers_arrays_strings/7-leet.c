5e5eec334a8f886d6e79bb8b71eb44cc579e94d2
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-16 13:32:32 +0000

    todo

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,24 @@
 #include"main.h"
 /**
-*reverse_array-asd
-*@a:asd
-*@n:asd
+*leet - asd
+*@c:asd
+*Return: 0
 **/
-char*leet(char *c)
+char *leet(char *c)
 {
 	int i = 0, a = 0;
 
-	char letters[]="aAeEoOtTlL";
-	char numbers[]="4433007711";
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
 
-	for(i = 0; c[i] != '\0' ;i++)
+	for (i = 0; c[i] != '\0'; i++)
 	{
 		for (a = 0; a < 10 ; a++)
 		{
-			if(c[i] == letters[a])
+			if (c[i] == letters[a])
 				c[i] = numbers[a];
 		}
 	}
 
-	return(c);
+	return (c);
 }
