27485faee27f685df94464df54b95ae4d29c0724
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-16 15:54:55 +0000

    todo

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,22 +1,19 @@
-<<<<<<< HEAD
 #include "main.h"
-=======
-#ZZinclude "main.h"
->>>>>>> d16758093711aaec10302ebd1c74486eb26c22f3
 /**
- *string_toupper - 
+*string_toupper - ssss
+* @str: ddd
+* Return: 0
  **/
-
 char *string_toupper(char *str)
-{	
+{
 	int cont = 0;
 
 	while (str[cont] != '\0')
 	{
-		 if (str[cont] >= 'a' && str[cont] <= 'z')
-		 {
+		if (str[cont] >= 'a' && str[cont] <= 'z')
+		{
 			str[cont] = str[cont] - 'a' + 'A';
-		 }
+		}
 	cont++;
 	}
 	return (str);
