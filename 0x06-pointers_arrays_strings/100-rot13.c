675aeab76d46d7cc52f2684d8395c01a2a6f562f
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-16 03:03:49 +0000

    readme

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *rot13 -
+ *@str: 
+ *Return:
+ **/
+char *rot13(char *str)
+{
+	int cont = 0 , i = 0;
+	char abc1[39]="ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char abc2[39]="abcdefghijklmñopqrstuvwxyzabcdefghijklm":
+
+	while (str[cont] != '\n')
+	{
+		while (str[cont])
+		{
+			
+		}	
+	}
+}
