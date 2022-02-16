7fcf5b5bf28a322a875f0b40ed40005e3b128c9e
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-16 16:49:56 +0000

    todo

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- *reverse_Array - 
- @*a:
- @n:
- **/
+*reverse_array - ddddd
+*@a: dddd
+*@n: ddd
+**/
 void reverse_array(int *a, int n)
-{	
+{
 	int b = 0;
-	int AUX;	
-	n-=1;
-	for (b = 0; b < (n)/2 ; b++, n--)
+	int AUX;
+
+	n -= 1;
+	for (b = 0; b < n / 2 ; b++, n--)
 	{
 		AUX = a[n];
 		a[n] = a[b];
