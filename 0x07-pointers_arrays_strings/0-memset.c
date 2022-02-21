c39c26941e070cfd7749580ab3f98eeba997e07b
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-21 15:00:05 +0000

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - convert
+ * @s: char
+ * @b: char 2
+ * @n: unsigned int
+ * Return: char
+**/
+char *_memset(char *s, char b, unsigned int n);
+{
+	int a = 0;
+
+	for (; a <= n; a++)
+	{
+		s[a] = b;
+	}
+return (s);
+}
