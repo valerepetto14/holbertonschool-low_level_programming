bc050df2027347ca64a3a3017ad28863d3322c6b
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-21 15:04:17 +0000

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,13 +6,12 @@
  * @n: unsigned int
  * Return: char
 **/
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
-	int a = 0;
+	unsigned int a = 0;
 
 	for (; a <= n; a++)
-	{
 		s[a] = b;
-	}
+
 return (s);
 }
