b7cc475f90b0a157b1ee48f15212a466a3d30c0f
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-23 15:16:27 +0000

    4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - potencia
+ * @x: int
+ * @y: int
+ * Return: int
+ **/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+	else
+		return (x * (_pow_recursion(x, y - 1)));
+}
