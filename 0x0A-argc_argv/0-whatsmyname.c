44488f4ea5254b613cf368d3833ec8ade98f5026
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-25 13:05:18 +0000

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *  main - ns
+ *  @argc: int
+ *  @argv: array
+ *  Return: 0
+**/
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
