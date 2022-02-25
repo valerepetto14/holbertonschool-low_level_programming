fb7d107fd0b1642f971ebcd7d63c42343e29c4fe
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-25 16:09:46 +0000

    todo

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,17 +7,15 @@
  *     *  Return: 0
  *     **/
 int main(int argc, char *argv[])
-{	
+{
 	if (argc <= 2)
 	{
-		printf("%d", argc);
 		printf("ERROR\n");
 		return (1);
 	}
 	else
-	{     
+	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
-    	
 	return (0);
 }
