006956d93b7b17916c4bc236bcc86f8a1f311b5a
valerepetto14 <valentinrepetto12345@gmail.com>
2022-02-25 16:01:44 +0000

    todo

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  *  main - ns
+ *   *  @argc: int
+ *    *  @argv: array
+ *     *  Return: 0
+ *     **/
+int main(int argc, char *argv[])
+{
+	int count;
+ if (argc > 0)
+    {
+      for (count = 0; count < argc; count++)
+	{
+	  printf("%s\n",argv[count]);
+	}
+    }
+return (0);
+}
