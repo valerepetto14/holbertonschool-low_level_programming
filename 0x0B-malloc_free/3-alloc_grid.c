8dd4b52e0e087f8607e68723c0c8f471563713f5
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-02 18:47:45 +0000

    4

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
 /**
  *alloc_grid - malloc
  *@width: int
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
  *free_grid - malloc
  *@grid:puntero a puntero
