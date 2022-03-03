1fdf34fe335f3f8ec68169c898d3d4069079ca23
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-03 16:07:39 +0000

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= strlen(s2))
 		n = strlen(s2) + 1;
 	if (n <= strlen(s2))
-		array[n + 1] = '\0';
+		array[largo1 + (n + 1)] = '\0';
 
 	for (; larray < largo1; larray++)
 		array[larray] = s1[larray];
