95b07f463e679a86daaf0d46cafc2f78949e0c41
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-03 17:25:55 +0000

    1

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,17 +9,19 @@
  **/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *array;
-	int i = 0;
+	char *array;
+	unsigned int i = 0;
 
-	if (nmemb == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
+
 	array = malloc(nmemb * size);
+
 	if (array == NULL)
 		return (NULL);
 
 	for (; i < nmemb; i++)
-		array[i] = 0;
+		*(array + i) = 0;
 
 	return (array);
 }
