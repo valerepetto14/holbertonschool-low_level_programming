6558cadadca41cd40d9a69af7d6ec700570343cc
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-08 15:23:42 +0000

    todo

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- *dog_t - struct
- **/
-typedef struct dog dog_t;
-/**
 *struct dog - struc
 *@name: name
 *@age: age
@@ -16,6 +12,11 @@ char * name;
 float age;
 char * owner;
 };
+
+/**
+*dog_t - struct  
+**/
+typedef struct dog dog_t;
 
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
