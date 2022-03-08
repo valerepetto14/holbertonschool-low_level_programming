a1b93c4df098046bd3fd2118d162a949bcec4f2c
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-08 15:52:03 +0000

    todo

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,13 +16,11 @@ void print_dog(struct dog *d)
 	if (d->age > 0)
 		printf("Age: %f\n", d->age);
 	else
-		printf("Age: (nil)\n");
+		printf("Age: %f\n", d->age);
 
 	if (d->owner != NULL)
 		printf("Owner: %s\n", d->owner);
 	else
 		printf("Owner: (nil)\n");
 	}
-	else
-		return;
 }
