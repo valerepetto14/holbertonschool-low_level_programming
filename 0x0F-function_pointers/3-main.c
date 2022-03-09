067ac8d03031ebfe4688cc9a56276f6b3f2a6c56
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-09 22:34:33 +0000

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,7 +18,6 @@ int main(int argc, char *argv[])
 			|| argv[2][0] != '/' || argv[2][0] != '%')
 	{
 		printf("Error\n");
-		exit(99);
 	}
 	if ((*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])) == 0)
 	{
