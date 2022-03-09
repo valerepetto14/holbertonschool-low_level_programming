4b351237d61c346f7d9dae7780c4d6309ecb4a3a
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-09 22:17:19 +0000

    ejercicio 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*get_op_func - dale
+*@s:char
+*Return: int
+**/
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+
+int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*ops[i].op == *s)
+			return (ops[i].f);
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,24 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - main
+*@argc: cont
+*@argv: array
+*Return: int
+**/
+int main(int argc, char *argv[])
+{
+	if (argc > 4 || argc < 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
