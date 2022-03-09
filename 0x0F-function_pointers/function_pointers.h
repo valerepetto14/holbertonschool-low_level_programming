44bdf500fe1b1ac464c5683fdf96bc5f9bfb1c3e
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-09 16:07:49 +0000

    todo

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#include <stddef.h>
 #ifndef FUNCTION_H
 #define FUNCTION_H
+#include <stddef.h>
 int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
