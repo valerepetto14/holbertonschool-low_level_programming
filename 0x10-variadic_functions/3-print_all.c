7ae7b2e01e369185eca4a55d13fc44a91584a5a5
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-11 01:41:49 +0000

    todo

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,41 +1,85 @@
 #include <stdio.h>
 #include <stdarg.h>
+void printchar(char *c);
+void printstr(char *is);
+void printint(char *i);
+void printfloat(char *f);
+
 /**
 *print_all - print
 *@format: char
 **/
 void print_all(const char * const format, ...)
 {
+
 	va_list ap;
-	char c;
-	char *s;
-	float f;
-	int i;
 	char *form = "";
+
 	*form = *format;
 
 	va_start(ap, format);
 	while (*form)
 	{
-	switch (*form++)
-	{
-	case 's':
-		s = va_arg(ap, char *);
-		printf("%s\n", s);
-		break;
-	case 'c':
-		c = (char) va_arg(ap, int);
-		printf("%c\n", c);
-		break;
-	case 'f':
-		f = va_arg(ap, int);
-		printf("%f\n", f);
-		break;
-	case 'i':
-		i = va_arg(ap, int);
-		printf("%d\n", i);
-		break;
-		}
+
 	}
 	va_end(ap);
+}
+
+/**
+ * printchar - p
+ *@c: c
+**/
+void printchar(char *c)
+{
+	printf("%c", c);
+}
+/**
+*get_op_func - dale
+*@s:char
+*Return: int
+**/
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+
+int i = 0;
+
+while (ops[i].op != NULL)
+	{
+		if (*ops[i].op == *s)
+			return (ops[i].f);
+		i++;
 	}
+	return (NULL);
+}
+/**
+ *printstr - p
+ *@s: c
+ **/
+void printstr(char *s)
+{
+	printf("%s", s);
+}
+/**
+ *printint - p
+ *@i: i
+ **/
+void printint(char *i)
+{
+	printf("%i", i);
+}
+/**
+ *printfloat - p
+ *@f: f
+ **/
+void printfloat(char *f)
+{
+	printf("%f", f);
+}
