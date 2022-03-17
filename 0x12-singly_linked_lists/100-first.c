76a197cfe895dd118e1ece98a70ade8f5cadeb28
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-17 19:45:52 +0000

    avanzado

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void myStartupFun(void) __attribute__ ((constructor));
+/**
+*myStartupFun - print
+**/
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,
+	\nI bore my house upon my back!\n");
+}
