41e0c283f538fe7b4654f3013bb9c3820513582e
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-17 15:39:42 +0000

    chamge

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@
  **/
 size_t print_list(const list_t *h)
 {
-	size_t n = 0;
+size_t n = 0;
 while (h != NULL)
 {
 	if (!h)
@@ -21,7 +21,7 @@ while (h != NULL)
 		printf("[0] (nil)\n");
 	}
 	else
-		 printf("[%d] (%s)\n", h->len, h->str);
+		 printf("[%d] %s\n", h->len, h->str);
 	h = h->next;
 	n++;
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,7 +2,12 @@
 #define LISTS_H
 #include <stddef.h>
 #include <stdlib.h>
-
+/**
+ *struct list - struct to use it later
+ *@str: symbol to print
+ *@len:number
+ *@next: pointer
+**/
 typedef struct list
 {
 	char *str;
