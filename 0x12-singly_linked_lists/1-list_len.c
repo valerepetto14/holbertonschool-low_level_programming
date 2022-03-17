ccdb3029b9e1f67febc6f15f0fa52dcdcf6fa95c
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-17 15:35:13 +0000

    chamge

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,15 @@
-i
+#include "list.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *list_len - printea
+ *@h: puntero
+ *Return: size
+ **/
+size_t list_len(const list_t *h)
+{
+ const list_t *aux = h;
+
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -9,6 +9,6 @@ typedef struct list
 	int len;
 	struct list *next;
 } list_t;
-
+size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 #endif
