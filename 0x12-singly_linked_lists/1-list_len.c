5e019d6619b396ba95a34b6cf643d778c0409195
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-17 15:43:54 +0000

    two

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -10,6 +10,12 @@
  **/
 size_t list_len(const list_t *h)
 {
- const list_t *aux = h;
+	size_t n = 0;
 
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
 }
