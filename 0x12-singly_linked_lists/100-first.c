676dcde3cff2590563bc4258423cad6148151eb3
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-17 19:57:20 +0000

    all

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include "lists.h"
 
-void myStartupFun(void) __attribute__ ((constructor));
 /**
 *myStartupFun - print
 **/
 void myStartupFun(void)
 {
-	printf("You're beat! and yet, you must allow,
-	\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/main.c
+++ 0x12-singly_linked_lists/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "lists.h"
 
 /**
  * main - check the code
