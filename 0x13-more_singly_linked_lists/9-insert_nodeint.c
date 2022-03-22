678e19710adea3c8b4ef9a2af9e86a1a258dbba9
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-22 15:34:49 +0000

    9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *insert_nodeint_at_index - printea
+ *@head: puntero
+ *@idx: unsigned int
+ *@n: num
+ *Return: pointer
+ **/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *aux = *head;
+	unsigned int iter = 0;
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	new_node->n = n;
+	if (*head == NULL && idx > 0)
+		return (NULL);
+	while (iter < idx)
+	{
+		if (!aux)
+			return (NULL);
+		aux = aux->next;
+	}
+	new_node->next = aux->next;
+	aux->next = new_node;
+	return (new_node);
+}
