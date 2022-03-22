ac6b7bce1a34a423df65aa4b69ce24cf690a300f
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-22 14:34:45 +0000

    6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,12 +10,14 @@
  *Return: int
  **/
 int pop_listint(listint_t **head)
-{
+{	
 	int date;
-
+	listint_t aux = *head;
+	
 	if (*head == NULL)
 		return (0);
 	date = (*head)->n;
 	(*head) = (*head)->next;
+	free(aux);
 	return (date);
 }
