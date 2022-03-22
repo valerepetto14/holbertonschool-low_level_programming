d66f89b1ac56a73f14c31b8ab47f73f6a409c8cd
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-22 16:56:41 +0000

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,6 +27,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	else if (*head == NULL && idx > 0)
 		return (NULL);
 
+	else if (head && idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
 	while (iter < idx - 1)
 	{
 		if (!aux)
