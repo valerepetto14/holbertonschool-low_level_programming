3be01dd9c144216e65bc9bc2910d9fbe6c603410
valerepetto14 <valerepetto12345@gmail.com>
2022-03-28 15:14:03 +0000

    add conditional exercise 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,15 +10,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd = open(filename, O_RDONLY);
 	char *buf = malloc(sizeof(char) * letters);
 	size_t cont = 0;
+	size_t validador = 0;
 
 	if (filename == NULL)
 		return (0);
 	if (fd == -1)
 		return (0);
 	if (buf == NULL)
+	{
+		close(fd);
 		return (0);
+	}
+
 	cont = read(fd, buf, letters);
-	write(0, buf, letters);
+	validador = write(0, buf, letters);
+	if (validador == 0)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 	close(fd);
 	return (cont);
 }
