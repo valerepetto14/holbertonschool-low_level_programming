95a7e5d911dd6eb5c1a6d38400bc1ed7559d8130
valerepetto14 <valerepetto12345@gmail.com>
2022-03-28 22:57:37 +0000

    change in the exercise

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,31 +7,29 @@
  **/
 int main(int argc, char *argv[])
 {
-	FILE *file1, *file2;
-	char c;
+	int file1, file2;
+	int cont;
+	char buf[1024];
 
-	file1 = fopen(argv[1], "r");
+	file1 = open(argv[1], O_RDONLY);
 
 	if (argc != 3)
 		return (97);
 
-	if (file1 == NULL)
+	if (file1 == -1)
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 
-	file2 = fopen(argv[2], "w");
-	if (file2 == NULL)
+	file2 = open(argv[2], O_WRONLY | O_CREAT, 0664);/**TRATO DE CREAR**/
+
+	if (file2 == -1)
 	{
-		fclose(file1);
+		close(file1);
 		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-	c = fgetc(file1);
-	while (c == EOF)
-		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
-	{
-		fputc(c, file2);
-		c = fgetc(file1);
-	}
-	fclose(file1);
-	fclose(file2);
+	cont = read(file1, buf, 1024);
+	close(file1);
+	write(file2, buf, cont);
+	close(file1);
+	close(file2);
 	return (0);
 }
