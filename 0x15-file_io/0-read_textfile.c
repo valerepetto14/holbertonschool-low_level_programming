e51490423428723c53e0529b858b15e607fcabab
valerepetto14 <valerepetto12345@gmail.com>
2022-03-28 15:04:16 +0000

    first commit exercise 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *read_textfile - read file a write
+ *@filename: string
+ *@letters: size
+ *Return: size_t
+ **/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd = open(filename, O_RDONLY);
+	char *buf = malloc(sizeof(char) * letters);
+	size_t cont = 0;
+
+	if (fd == -1)
+		return (0);
+	if (buf == NULL)
+		return (0);
+	cont = read(fd, buf, letters);
+	write(0, buf, letters);
+	close(fd);
+	return (cont);
+}
