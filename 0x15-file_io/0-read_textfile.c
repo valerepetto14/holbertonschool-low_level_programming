0b286794d78efbe3c9304cf7cd439b3ea4a9d093
valerepetto14 <valerepetto12345@gmail.com>
2022-03-28 16:07:09 +0000

    add conditional exercise 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,15 +7,19 @@
  **/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd = open(filename, O_RDONLY);
-	char *buf = malloc(sizeof(char) * letters); /**TRATO DE ABRIR EL ARCHIVO**/
 	size_t cont = 0;
 	size_t validador = 0;
 
 	if (filename == NULL)
 		return (0);
+
+	int fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
+
+	char *buf = malloc(sizeof(char) * letters); /**TRATO DE ABRIR EL ARCHIVO**/
+
 	if (buf == NULL)
 	{
 		close(fd);
@@ -24,13 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	cont = read(fd, buf, letters); /** TRATO DE LEER EL ARCHIVO **/
 	validador = write(0, buf, letters);/** TRATO DE ESCRIBIR EL CONTENIDO **/
-	if (validador == 0)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
-	if (cont != validador)
+	if (validador != cont)
 		return (0);
 	close(fd);
 	free(buf);
