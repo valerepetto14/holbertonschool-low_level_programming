c4201eb5ba03ea39049ee2ffdf2121e0e686f67b
valerepetto14 <valerepetto12345@gmail.com>
2022-03-28 18:39:04 +0000

    exercise 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,15 +14,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	archivo = open(filename, O_WRONLY | O_CREAT, 0600);
+	archivo = open(filename, O_WRONLY | O_CREAT, 0600);/**TRATO DE CREAR**/
 	if (archivo == -1)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-	fp = fopen(filename, "w+");
-	for (; text_content[iter] != '\0'; iter++)
+	fp = fopen(filename, "w+");/**TRATO DE ABRIR EL ARCHIVO**/
+	for (; text_content[iter] != '\0'; iter++)/**ESCRIBO DENTRO DEL ARCHIVO**/
 	{
-		validador = fputc(text_content[iter], fp);
+		validador = fputc(text_content[iter], fp); /**ESCRIBO DE A UN CARACTER**/
 		if (validador == -1)
 			return (-1);
 	}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *append_text_to_file - append texto a file
+ *@filename: string
+ *@text_content: contenido
+ *Return:int
+ **/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	FILE *fp;
+	int iter = 0, archivo = 0, validador = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (archivo == -1)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	fp = fopen(filename, "a");
+	if (fp == NULL)
+		return (-1);
+	for (; text_content[iter] != '\0'; iter++)
+	{
+		validador = fputc(text_content[iter], fp);
+		if (validador == -1)
+			return (-1);
+	}
+	fclose(fp);
+	return (1);
+}
