a7d15ed14c52312b4a55bfcca3de4d4ca911cf0d
valerepetto14 <valerepetto12345@gmail.com>
2022-03-28 18:07:12 +0000

    change

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,16 +10,15 @@ int create_file(const char *filename, char *text_content)
 {
 	FILE *fp;
 	int iter = 0, archivo = 0;
+	
+	if (filename == NULL)
+		return (-1);
 
 	archivo = open(filename, O_WRONLY | O_CREAT, 0600);
 	if (archivo == -1)
 		return (-1);
-	if (filename == NULL)
-		return (-1);
-	fp = fopen(filename, "w+");
 
-	if (fp == NULL)
-		return (-1);
+	fp = fopen(filename, "w+");
 	for (; text_content[iter] != '\0'; iter++)
 		fputc(text_content[iter], fp);
 	fclose(fp);
