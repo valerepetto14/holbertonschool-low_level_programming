f487625a31f521c9bb07616c59dbe3b905f4a0c3
valerepetto14 <valerepetto12345@gmail.com>
2022-03-28 18:14:53 +0000

    change

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,11 +13,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-
+	
 	archivo = open(filename, O_WRONLY | O_CREAT, 0600);
 	if (archivo == -1)
 		return (-1);
-
+	if (text_content == NULL)
+		text_content = "";
 	fp = fopen(filename, "w+");
 	for (; text_content[iter] != '\0'; iter++)
 	{
