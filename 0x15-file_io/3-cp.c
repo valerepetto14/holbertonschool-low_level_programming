6c2df46e75769efdc721e7eac201138b56d8fce6
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-29 01:59:19 +0000

    the last change

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,20 +11,18 @@ int main(int argc, char *argv[])
 	int cont = 0, writes = 0, close1 = 0;
 	char buf[1024];
 
-	file1 = open(argv[1], O_RDONLY);
 	if (argc != 3)
-	{
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	file1 = open(argv[1], O_RDONLY);
 	if (file1 == -1)
 	{
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
-	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);/**TRATO DE CREAR**/
+	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file2 == -1)
 	{
-		close(file1);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
 	while ((cont = read(file1, buf, 1024)) != 0)
 	{
@@ -33,7 +31,7 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		writes = write(file1, buf, cont);
+		writes = write(file2, buf, cont);
 		if (writes == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -42,9 +40,9 @@ int main(int argc, char *argv[])
 	}
 	close1 = close(file1);
 	if (close1 == -1)
-		dprintf(2, "Error: Can't close fd %d\n", file1), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1), exit(100);
 	close1 = close(file2);
 	if (close1 == -1)
-		dprintf(2, "Error: Can't close fd %d\n", file2), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2), exit(100);
 	return (0);
 }
