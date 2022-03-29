80507f9ec28168fd145c0f47d5f92275979472ce
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-29 01:54:04 +0000

    the last change

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
 int main(int argc, char *argv[])
 {
 	int file1 = 0, file2 = 0;
-	int cont, writes, close1;
+	int cont = 0, writes = 0, close1 = 0;
 	char buf[1024];
 
 	file1 = open(argv[1], O_RDONLY);
@@ -16,8 +16,7 @@ int main(int argc, char *argv[])
 	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	if (file1 == -1)
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	file2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);/**TRATO DE CREAR**/
-
+	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);/**TRATO DE CREAR**/
 	if (file2 == -1)
 	{
 		close(file1);
@@ -39,7 +38,7 @@ int main(int argc, char *argv[])
 	}
 	close1 = close(file1);
 	if (close1 == -1)
-		dprintf(2, "Error: Can't close fd1 %d\n", file1), exit(100);
+		dprintf(2, "Error: Can't close fd %d\n", file1), exit(100);
 	close1 = close(file2);
 	if (close1 == -1)
 		dprintf(2, "Error: Can't close fd %d\n", file2), exit(100);
