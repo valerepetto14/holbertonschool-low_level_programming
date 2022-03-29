e30dcf5159b6a1a700214e57c98606f3dd71b0c8
valerepetto14 <valentinrepetto12345@gmail.com>
2022-03-29 01:55:37 +0000

    the last change

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,9 +13,13 @@ int main(int argc, char *argv[])
 
 	file1 = open(argv[1], O_RDONLY);
 	if (argc != 3)
+	{
 	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	}
 	if (file1 == -1)
+	{
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	}
 	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);/**TRATO DE CREAR**/
 	if (file2 == -1)
 	{
