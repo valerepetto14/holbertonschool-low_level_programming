4f578a47d2fc76409fa46045e6b59b870ba8e167
valerepetto14 <valerepetto12345@gmail.com>
2022-05-05 19:47:39 +0000

    1 checkers

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,7 +8,6 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *aux = NULL;
-	dlistint_t *borrar = NULL;
 	unsigned int i = 0;
 
 	if (*head == NULL || head == NULL)
