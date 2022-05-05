22d4267e0fc61f0b086edc535b61b82a8dc9c224
valerepetto14 <valerepetto12345@gmail.com>
2022-05-05 17:31:59 +0000

    anda a medias

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,15 +12,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *aux = *h;
 	unsigned int iter = 0;
 
+	if (*h == NULL || h == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
-		return (NULL);
-	if (*h == NULL || h == NULL)
-		return (NULL);
 	if (idx == 0)
 	{
 		new_node->next = *h;
