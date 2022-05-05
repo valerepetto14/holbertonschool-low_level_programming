71c9b5d2e7ea98649cd27636ea2ea4b5cbc2c2a1
valerepetto14 <valerepetto12345@gmail.com>
2022-05-05 15:45:47 +0000

    4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ *free_dlistint - main
+ *@head: head
+ **/
+void free_dlistint(dlistint_t *head)
+{
+	while (head != NULL)
+	{
+		aux = head
+		head = head->next;
+		free(aux);
+	{
+}
