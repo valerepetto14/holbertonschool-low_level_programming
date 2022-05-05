24d0299807914e6efb0828a9e8c7a3e027a0b774
valerepetto14 <valerepetto12345@gmail.com>
2022-05-05 18:05:53 +0000

    tratando

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,16 +1,4 @@
 #include "lists.h"
- 
-unsigned int lenlist(dlistint_t *head)
-{
-	unsigned int len = 0;
-	dlistint_t *aux = head;
-	while (aux->next != NULL)
-	{
-		aux = aux->next;
-		len++;
-	}
-	return (len);
-}
 /**
  **insert_dnodeint_at_index - main
  *@h: lista
@@ -25,15 +13,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int iter = 0;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL || h == NULL)
-		return (NULL);
-	if (idx > lenlist(*h) + 1)
+	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
 	if (idx == 0)
 	{
+		if (h == NULL)
+		{
+			*h = new_node;
+			return (new_node);
+		}
 		new_node->next = *h;
 		(*h)->prev = new_node;
 		*h = new_node;
@@ -41,10 +32,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	while (iter < idx - 1)
 	{
+		if (aux->next  == NULL)
+			return (NULL);
 		aux = aux->next;
 		iter++;
-		if (aux == NULL)
-			return (NULL);
 	}
 	if (aux->next == NULL)
 	{
