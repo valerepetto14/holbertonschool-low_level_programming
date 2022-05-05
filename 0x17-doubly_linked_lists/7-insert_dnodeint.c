4ba8aad3c8aa412f0cbd070f3bda8e9564cbf75f
valerepetto14 <valerepetto12345@gmail.com>
2022-05-05 17:14:00 +0000

    add end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
-
+	if (idx < 0)
+		return (NULL);
 	if (idx == 0)
 	{
 		new_node->next = *h;
