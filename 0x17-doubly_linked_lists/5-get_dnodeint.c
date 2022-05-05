30ddab9a0ba3a3fc62d63395b457a1118bc38396
valerepetto14 <valerepetto12345@gmail.com>
2022-05-05 16:03:22 +0000

    add 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *get_dnodeint_at_index - main
+ *@head: head
+ *@index: indice
+ *Return: lista
+ **/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int iter = 0;
+	listint_t *aux = head;
+
+	while (aux)
+	{
+		if (iter == index)
+			return (aux);
+		iter++;
+		aux = aux->next;
+	}
+	return (NULL);
+}
+
