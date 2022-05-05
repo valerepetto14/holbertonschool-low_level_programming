c09221d510814d73c500a4a64cc1d1839978939e
valerepetto14 <valerepetto12345@gmail.com>
2022-05-05 16:51:21 +0000

    prev en aux

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		aux = aux->next;
 		iter++;
 	}
-
+	aux= aux->prev;
 	new_node->next = aux->next;
 	new_node->prev = aux;
 	aux->next = new_node;
