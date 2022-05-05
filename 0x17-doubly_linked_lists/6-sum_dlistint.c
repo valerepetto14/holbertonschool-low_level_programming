17d7a7183e88c6e47d475e6d202c389e2fee9985
valerepetto14 <valerepetto12345@gmail.com>
2022-05-05 16:17:06 +0000

    add 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_dlistint - main
+ *@head: lista
+ *Return: int
+ **/
+int sum_dlistint(dlistint_t *head)
+{
+	int suma = 0;
+	dlistint_t *aux = head;
+
+	while (aux->prev != NULL)
+		aux = aux->prev;
+	while (aux->next != NULL)
+	{
+		aux = aux->next;
+		suma += aux->n;
+	}
+	return (suma);
+}
