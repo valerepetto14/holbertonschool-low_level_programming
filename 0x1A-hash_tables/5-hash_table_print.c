49dc66ad3fa53e2982aa694018886cd41582385c
valerepett14 <valentinrepetto12345@gmail.com>
2022-05-27 03:09:33 +0000

    fix 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -17,7 +17,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (cont != 0)
 				printf(", ");
-			printf("'%s' : '%s'", actual->key, actual->value);
+			printf("'%s': '%s'", actual->key, actual->value);
 			actual = actual->next;
 			cont++;
 		}
