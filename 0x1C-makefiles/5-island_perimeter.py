a3504d3165af403f6322241457335e54ad89aefe
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-07-14 15:17:55 +0000

    add 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,3 +1,13 @@
 def island_perimeter(grid):
-    for i in grid:
-        
+    """def function"""
+    m,n=len(grid), len(grid[0])
+        land, nei=0,0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1:
+                    land+=1
+                    if i < m-1 and grid[i+1][j]==1:
+                        nei+=1
+                    if j < n-1 and grid[i][j+1]==1:
+                        nei+=1
+        return land*4-2*nei
